f87065b6dfafe4c237941364c9d91ac788022a7d
LeeSeungHyun9661 <dltmdgus9661@gmail.com>
2023-12-04 00:23:56 +0900

    Rebuild

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
+// 상단 메뉴바 - 버튼 클릭으로 해당 세션 이동
 $('.nav-link').click(function (e) {
    var href = $(this).attr("href");
-   var targetTop = $(href).offset().top -55 ;
+   var targetTop = $(href).offset().top -60 ;
    $("html").stop().animate({ scrollTop : targetTop }, 300);
    e.preventDefault();
 });
 
+// 상단 메뉴바 - 버튼 상태 업데이트 / 오프셋 값에 따라 
 function Page__updateIndicatorActive() {
    var scrollTop = $(window).scrollTop();
-   // 역순으로 검색해야 편하다
    $($(".section").get().reverse()).each(function (index, node) {
       var offsetTop = parseInt($(this).attr("data-offset-top"));
       if (scrollTop >= offsetTop) { 
@@ -19,19 +20,16 @@ function Page__updateIndicatorActive() {
 
          $("html").attr("data-current-page-index", currentPageIndex);
 
-         return false; // 더 이상 다른 페이지를 검사하지 않는다.
-      }else{
-         console.log("scrollTop:" + scrollTop);
-         console.log("offsetTop:" + offsetTop);
+         return false;
       }
    });
 }
 
-// 각 페이지의 offsetTop 속성을 업데이트
+// 상단 메뉴바 - 버튼 상태 업데이트 / 오프셋 값 변경
 function Page__updateOffsetTop() {    
     $(".section").each(function (index, node) {
          var $page = $(node);
-         var offsetTop = $page.offset().top - 55;
+         var offsetTop = $page.offset().top - 60;
          $page.attr("data-offset-top", offsetTop);
     });    
     // 계산이 바뀌었으니까, 다시 상태 업데이트
@@ -42,7 +40,7 @@ function Page__init() {
     Page__updateOffsetTop();
 }
 
-// 초기화
+// 페이지 초기화로 상단 메뉴바 버튼 업데이트
 Page__init();
 
 // 화면이 리사이즈 할 때 마다, offsetTop을 다시계산
@@ -50,3 +48,5 @@ $(window).resize(Page__updateOffsetTop);
 
 // 스크롤이 될 때 마다, 인디케이터의 상태를 갱신
 $(window).scroll(Page__updateIndicatorActive);
+
+// 참고 출처 : https://codepen.io/kimyangsun/pen/vYEqWee
