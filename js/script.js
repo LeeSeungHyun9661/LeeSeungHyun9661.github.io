c4a31d391e872326af7ac6d7685b9b3151ae3e31
LeeSeungHyun9661 <dltmdgus9661@gmail.com>
2023-12-09 14:50:30 +0900

    skills filled

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ $('.nav-link').click(function (e) {
    $("html").stop().animate({ scrollTop : targetTop }, 300);
    e.preventDefault();
 });
-
 // 상단 메뉴바 - 버튼 상태 업데이트 / 오프셋 값에 따라 
 function Page__updateIndicatorActive() {
    var scrollTop = $(window).scrollTop();
@@ -24,7 +23,6 @@ function Page__updateIndicatorActive() {
       }
    });
 }
-
 // 상단 메뉴바 - 버튼 상태 업데이트 / 오프셋 값 변경
 function Page__updateOffsetTop() {    
     $(".section").each(function (index, node) {
@@ -39,16 +37,15 @@ function Page__updateOffsetTop() {
 function Page__init() {
     Page__updateOffsetTop();
 }
-
-// 페이지 초기화로 상단 메뉴바 버튼 업데이트
+// 상단 메뉴바 - 페이지 초기화로 상단 메뉴바 버튼 업데이트
 Page__init();
 
-// 화면이 리사이즈 할 때 마다, offsetTop을 다시계산
+// 상단 메뉴바 - 화면이 리사이즈 할 때 마다, offsetTop을 다시계산
 $(window).resize(Page__updateOffsetTop);
 
-// 스크롤이 될 때 마다, 인디케이터의 상태를 갱신
+// 상단 메뉴바 - 스크롤이 될 때 마다, 인디케이터의 상태를 갱신
 $(window).scroll(Page__updateIndicatorActive);
-
 // 참고 출처 : https://codepen.io/kimyangsun/pen/vYEqWee
 
+// flipster
 var coverflow = $("#coverflow").flipster();
